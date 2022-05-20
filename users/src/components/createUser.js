3dded678d086ace5f133a550d50283924961261d
MONICAMATHIYALAGAN <monicamoni806@gmail.com>
2022-05-20 22:54:19 +0530

    initial

--- users/src/components/createUser.js
+++ users/src/components/createUser.js
@@ -23,9 +23,58 @@ const CreateUser = () => {
               value={name}
               onChange={(event)=>setName(event.target.value)}
               className="form-control"
-              placeholder="Enter Name"
+              placeholder="Enter Your Name"
             />
-          </div>
+        </div>
+        <div className="form-group">
+            <label htmlFor="email" style={{color:"black"}}>Email ID:</label>
+            <input
+              style={{ width: "40%" }}
+              type="text"
+              id="email"
+              value={email}
+              onChange={(event)=>setEmail(event.target.value)}
+              className="form-control"
+              placeholder="Enter Your Email ID"
+            />
+        </div>
+        <div className="form-group">
+            <label htmlFor="mobileNumber" style={{color:"black"}}>Mobile Number:</label>
+            <input
+              style={{ width: "40%" }}
+              type="text"
+              id="mobileNumber"
+              value={mobileNumber}
+              onChange={(event)=>setMobileNumber(event.target.value)}
+              className="form-control"
+              placeholder="Enter your Mobile Number"
+            />
+        </div>
+        <div className="form-group">
+            <label htmlFor="gender" style={{color:"black"}}>Gender:</label>
+            <input
+              style={{ width: "40%" }}
+              type="text"
+              id="gender"
+              value={gender}
+              onChange={(event)=>setGender(event.target.value)}
+              className="form-control"
+              placeholder="Enter Your Gender"
+            />
+        </div>
+        <div className="form-group">
+            <label htmlFor="games" style={{color:"black"}}>Games:</label>
+            <input
+              style={{ width: "40%" }}
+              type="text"
+              id="games"
+              value={name}
+              onChange={(event)=>setGames(event.target.value)}
+              className="form-control"
+              placeholder="Enter Your Games"
+            />
+        </div>
+
         </form>
 
         </>
